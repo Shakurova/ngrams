6c28190af503fbb164eaa6463a71f9834307c8a6
Shakurova <lenashakurova@yandex.ru>
2016-03-23 02:28:26 +0400

    Added files via upload

--- ngrams.py
+++ ngrams.py
@@ -1,68 +1,60 @@
 # -*- coding: utf-8 -*-
-import re, codecs, sys, time
+import codecs, re, sys, time
 
+googlefile = u'googlebooks-rus-all-2gram-20120701-vl' #Заменить
+dictionary = {}
 
-googlefile = u'googlebooks-rus-all-2gram-20120701-vl'
-arr_word = []
-dictionary = {} # 
-count_line = 0 # How many lines have already been processed
-size = 1097621 # КБ
-##print sum(1 for l in open(googlefile, "r")) #26274744 - lines
+#Проверять, что сначала ADJ, потом NOUN
+#Проверять согласование
+#Засекать время
+#Сделать для разных языков
 
-
-
-##def cleaning():
-##    with codecs.open(googlefile, 'r', 'utf-8') as f:
-##        for line in f:
-##            re.sub("[^а-яА-ЯёЁ ]", "", line)
-
-##cleaning()
-
-
-    
-
-def frequency():
+def preprocessing(googlefile):
     count_line = 0
-    #t1 = time.time()
+    prepfile = codecs.open('prepfile.tsv', 'w', 'utf-8')
     with codecs.open(googlefile, 'r', 'utf-8') as f:
         for line in f:
-            if u"влажный" in line or u"ВЛАЖНЫЙ" in line:
+            if u"влажн" in line and u"ADJ" in line and u"NOUN" in line:
                 #sys.stdout.write(line) # All lines that contain u"влажный"
-                splited_line = re.split("\s|\t", line)
+                prepfile.write(line)
                 count_line += 1
-                if count_line == 2000:
-                     print count_line
-                     #print(time.time() - t1)
-                     count_line = 0
-                #sys.stdout.write(splited_line[1]+ "\r\n")
-                c = 0
-                if splited_line[1] not in dictionary:
-                    dictionary[splited_line[1]] = 1
-                    #sys.stdout.write(splited_line[1] + " " + str(dictionary[splited_line[1]]) + "\r\n")
-                else:
-                    c = dictionary[splited_line[1]]
-                    c += 1   
-                    dictionary[splited_line[1]] = c
-                    #sys.stdout.write(splited_line[1] + " " + str(dictionary[splited_line[1]]) + "\r\n")
-      
+                if count_line % 2000 == 0:
+                    print (count_line)
+    prepfile.close()
+    return (prepfile)
 
-frequency()
+def frequency(testfile):
+    count_line = 0
+    with codecs.open(testfile, 'r', 'utf-8') as f:
+        for line in f:
+            splited_line = line.split()
+            #sys.stdout.write(splited_line[0] + " " + splited_line[1] + " " + splited_line[3] + "\r\n")
+            aa = splited_line[1].split('_')[0]
+            if aa not in dictionary:
+                dictionary[aa] = int(splited_line[3])
+                #sys.stdout.write(splited_line[1] + " " + str(dictionary[splited_line[1]]) + "\r\n")
+            else:
+                dictionary[aa] += int(splited_line[3])
+    return (dictionary)
+
+#prepfile = preprocessing(googlefile)
+prepfile = u'prepfile.tsv'
+frequency(prepfile)
+
+##Запись в файл ngrams_result.tsv в формате существительное + число вхождений
+w = codecs.open(u'test_result.tsv', 'w', 'utf-8')
+for i in sorted(dictionary, key=dictionary.get, reverse=True):
+    w.write(i +'\t'+ str(dictionary[i]) + "\r\n")
+w.close()
 
 
-dictionary.sort()
-most = 0
-for word in dictionary:
-    while most < 30:
-        sys.stdout.write(word + " " + str(dictionary[word]) + "\r\n")
-        most += 1 
-    
-# Нужно ли вообще записывать словарь в файл? Наверное да ...
-##w = codecs.open(u'ngrams_result.txt', 'w', 'utf-8')
-##for d in dictionary:
-##    w.write(d + "\r\n")
-##w.close()
 
-##1. Можно ли сделать очистку текста от всего что не кирилица не с помощью регулярных выражений? Как это сделать быстрее?
-##2. прикинуть, какому объёму в мегабайтах соответствует какое количество строк в файле с биграммами, чтобы уметь предсказывать время работы программы.
-##3. 2000 строк обрабатываются примерно за 20 секунд, всего  26274744 строк, значит весь файл будет обрабатываться 72 часа (если я правильно посчитала).
-## Как можно ускорить работу программы?
+
+##Описание работы программы
+
+##preprocessing
+##Сначала создается файл из строк, содержащих основу нужного прилагательного и теги Adj Noun (порядок пока не учитывается)
+
+##frequency
+##Строки делятся по пробелу, а потом по подчеркиванию. Получается массив splited_line = мокрая, Adj, вата, Noun, 1994, 45
+##Создается словарь dictionary. если слово есть в словаре, прибавляется число вхождений, если нет - присваивается

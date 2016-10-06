b5daaa1cf0e48d7923350141260ef5947e952710
Elena Shakurova <lenashakurova@yandex.ru>
2016-10-07 00:28:46 +0300

    german and english

--- english/english.py
+++ english/english.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import eng_config
 import eng_lines_selector
 import eng_ngrams
--- german/german.py
+++ german/german.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import ge_config
 import ge_lines_selector
 import ge_ngrams
